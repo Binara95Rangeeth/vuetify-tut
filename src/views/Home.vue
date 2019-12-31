98d7778c7825733ff49a40ec514a31853bb0b755
Binara <binna.ranga@gmail.com>
2019-12-31 09:41:34 +0530

    text and color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home page</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quam minima quae cum, fuga quasi voluptates optio, excepturi vel perspiciatis fugiat necessitatibus eos illo porro recusandae non explicabo aut dolor.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos et in minima magnam impedit voluptas officia at quas iusto? Iure perspiciatis earum voluptate. Incidunt odit omnis nihil consequatur architecto labore!</p>
+    <h1 class="display-4 font-weight-black">Massive text</h1>
+    <h4 class="display-1">smaller text</h4>
+    <p class="headline">Headline</p>
+    <p class="subheading">Subheading</p>
+    <p class="caption">This is caption</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
